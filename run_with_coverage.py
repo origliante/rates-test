1b484774051675a7950409e5370b979d54adba60
Michele <michelefrettoli@gmail.com>
2019-01-24 19:26:38 +0100

    run_with_coverage and make combine work

---
+++ run_with_coverage.py
@@ -0,0 +1,20 @@
+import os
+
+from coverage import Coverage
+
+import api
+
+
+cov = Coverage(data_file='.coverage_integration')
+cov.start()
+
+@api.app.route('/quit', methods=['GET'])
+def quit():
+    global cov
+    cov.stop()
+    cov.save()
+    os.system('kill %d' % os.getpid())
+
+if __name__ == '__main__':
+    api.app.run(debug=True)
+
